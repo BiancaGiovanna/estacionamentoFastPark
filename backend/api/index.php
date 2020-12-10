22383ded0bbff49c6c8818cc3fcec127e29b64a4
LUCAS ALVES MENDES <20185482@CFP127ALUNO.LOCAL>
2020-12-10 15:24:52 -0300

    criei a index da api e a pasta model

--- backend/api/index.php
+++ backend/api/index.php
@@ -6,7 +6,10 @@ require_once("vendor/autoload.php");
 $app = new \Slim\App();
 
 $app->get('/', function($request, $response, $args){
-    return $response-getBody()->write('Bem vindo a API');
+    return $response->getBody()->write('Bem vindo a API!!');
+});
+$app->post('/', function ($request, $response, $args){
+
 });
 
 //carrega todos os endPoints

8874133545a370c3e226bbbbaba391813a5abed5
LUCAS ALVES MENDES <20185482@CFP127ALUNO.LOCAL>
2020-12-14 16:55:06 -0300

    commit do endPoint Post

---
+++ backend/model/apiPost.php
@@ -0,0 +1,39 @@
+<?php
+
+function insertMovimento($dadosMovimento){
+    require_once('../controller/connectionMysql.php');
+
+    require_once('../controller/settings.php');
+
+    if(!$conex = connectionMysql()){
+        echo("<script> alert('".ERRO_CONEX_BD_MYSQL."'); </script>");
+    }
+
+    $placa = (string) null;
+    $dataEntrada = (string) null;
+    $codComprovante = (string) null;
+    $statusCliente = (boolean) 1;
+
+    $placa = $dadosMovimento['placa'];
+
+    $sql = "insert into tblmovimento
+            (
+            placa,
+            dataEntrada,
+            codComprovante,
+            statusCliente
+            ) values 
+            (
+                '".$placa."',
+                current_timestamp(),
+                concat_ws('-', second(curtime()), hour(curtime()), second(curtime()), month(curdate()), minute(curtime()), month(curdate())),
+                '".$statusCliente."'
+            )";
+}
+function convertJSON($obj){
+    header("content-type:application/json");
+
+    $listJSON = json_encode($obj);
+
+    return $listJSON;
+}
